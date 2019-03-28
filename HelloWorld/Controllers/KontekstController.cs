9b9186d0a02bd544b64e26626fd2c04efd385d77
Runtić Ivan <runta_osk@hotmail.com>
2019-03-28 18:56:57 +0100

    zadatak 3.1.2 i 3.1.3

--- HelloWorld/Controllers/KontekstController.cs
+++ HelloWorld/Controllers/KontekstController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace HelloWorld.Controllers
 {
@@ -16,6 +17,44 @@ namespace HelloWorld.Controllers
             //return View();
         }
 
+        public string QueryPodaci()
+        {
+            if (Request.QueryString["Ime"] == null || Request.QueryString["Prezime"] == null)
+            {
+                return "Podaci su nepotpuni!";
+            }
+            else
+            {
+                string ime = Request.QueryString["Ime"];
+                string prezime = Request.QueryString["Prezime"];
+                string tekstSaStilom = string.Format(
+                    "<label style='color:red'; font-weight:bold;'>{0} {1}</label>",
+                    ime, prezime);
+                return string.Format(
+                    "<p>Ime i prezime iz query stringa su: {0}</p>",
+                    tekstSaStilom);
+            }
+        }
 
+        public string RoutePodaci()
+        {
+            try
+            {
+                string kontroler = RouteData.Values["controller"].ToString();
+                string akcijskaMetoda = RouteData.Values["action"].ToString();
+                string parametarId = RouteData.Values["id"].ToString();
+                return "<h1>Route podaci:</h1>" +
+                    "Kontroler: " + kontroler + "<br />" +
+                    "Metoda: " + akcijskaMetoda + "<br />" +
+                    "Parametar id: " + parametarId + "<br />";
+            }
+            catch (Exception ex)
+            {
+                return "Došlo je do pogreške: " + ex.Message;
+            }
+        }
     }
+
+
 }
+
