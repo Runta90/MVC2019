c2a225c1ef31c0c4422bd13978de5d4c7097ca33
Runtić Ivan <runta_osk@hotmail.com>
2019-03-28 18:10:17 +0100

    zbirka 2.1.1, 3.1.1

---
+++ HelloWorld/Controllers/Home1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Home1
+        public ActionResult Index()
+        {
+            string message = "Rezultat operacije 4 + 3 * 3 = ";
+            return View((object)message);
+        }
+    }
+}
---
+++ HelloWorld/Controllers/KontekstController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class KontekstController : Controller
+    {
+        // GET: Kontekst
+        public string Index()
+        {
+            string poruka = "Danas je: " + DateTime.Now.ToLongDateString();
+            return poruka;
+            //return View();
+        }
+
+
+    }
+}
