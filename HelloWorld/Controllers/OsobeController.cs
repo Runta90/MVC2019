bde97bf2804d57e7d32e1234203599f66a246c45
Runtić Ivan <runta_osk@hotmail.com>
2019-03-29 18:41:12 +0100

    modeli 4.2

---
+++ HelloWorld/Controllers/OsobeController.cs
@@ -0,0 +1,26 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class OsobeController : Controller
+    {
+        // GET: Osobe
+        public ViewResult PopuniOsobu()
+        {
+            return View();
+        }
+
+        //POSt: Osoba/PopuniOsobu
+        [HttpPost]
+        public ActionResult PopuniOsobu(Osoba osoba)
+        {
+            return View(osoba);
+        }
+
+    }
+}
