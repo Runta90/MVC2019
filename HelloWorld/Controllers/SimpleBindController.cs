106cd40a2c9ecd9556d61a1c40b0765c71da090a
Runtić Ivan <runta_osk@hotmail.com>
2019-03-29 17:34:55 +0100

    4.1.1 modeli

---
+++ HelloWorld/Controllers/SimpleBindController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class SimpleBindController : Controller
+    {
+        // GET: SimpleBind
+        public ActionResult SimpleBindMetoda()
+        {
+            return View("SimpleBindMetoda");
+        }
+
+        [HttpPost]
+        public ViewResult SimpleBindMetoda(string ime)
+        {
+            string pozdrav = "Pozdrav, " + ime + "!";
+            return View("SimpleBindMetoda", (object)pozdrav);
+        }
+    }
+}
