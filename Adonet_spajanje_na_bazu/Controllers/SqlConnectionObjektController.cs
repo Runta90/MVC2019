2e85d0b2cd611573283e73c1b0dcd2cec293a189
Runtić Ivan <runta_osk@hotmail.com>
2019-04-08 18:04:54 +0200

    Web config primjer

--- Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjektController.cs
+++ Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjektController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,7 @@ namespace Adonet_spajanje_na_bazu.Controllers
 {
     public class SqlConnectionObjektController : Controller
     {
-        // GET: SqlConnectionObjekt
+        // GET: SqlConnectionObjekt direktno
         public ActionResult Index()
         {
             // Prvo kreiramo conn string
@@ -23,6 +24,42 @@ namespace Adonet_spajanje_na_bazu.Controllers
             {
                 conn.Open();
                 if(conn.State == System.Data.ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela.");
+                }
+            }
+            catch (SqlException sqex)
+            {
+                Response.Write("Greška spajanja na bazu" + sqex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("Neka greška" + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+
+        // GET: SqlConnectionObjekt preko WEB.config
+        public ActionResult SpojPrekoWebKonfiga()
+        {
+            // Prvo kreiramo conn string
+            // string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+
+            // Nakon toga instanca Sqlconnection
+            SqlConnection conn = new SqlConnection(connString);
+
+            // Otvaramo vezu s bazom
+            try
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
                 {
                     Response.Write("Konekcija je uspjela.");
                 }
