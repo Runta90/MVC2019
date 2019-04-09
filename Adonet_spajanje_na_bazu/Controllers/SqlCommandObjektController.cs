8bbce229f6e0a433919e22761884d6cf2d7876c3
Runtić Ivan <runta_osk@hotmail.com>
2019-04-09 17:46:56 +0200

    sql command (nedovrseno)

---
+++ Adonet_spajanje_na_bazu/Controllers/SqlCommandObjektController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adonet_spajanje_na_bazu.Controllers
+{
+    public class SqlCommandObjektController : Controller
+    {
+        // GET: SqlCommandObjekt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            // Prvo kreiramo conn string
+            // string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+
+            // Nakon toga instanca Sqlconnection
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string cmdTxt = "";
+                cmdTxt += "INSERT INTO ";
+            }
+
+            return View();
+        }
+}
